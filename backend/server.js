b8661681e8b7992257951f9d4d5849861643b220
Udbhav <audbhav22@iitk.ac.in>
2026-01-22 17:04:42 +0000

    backend: raise ingest json limit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 
 const ingestRoutes = require('./routes/ingestRoutes');
 const analyticsRoutes = require('./routes/analyticsRoutes');
